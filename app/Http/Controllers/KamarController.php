40056d9085044dff09ec2e9791125a2b09caa087
TRI SETIANTO <156288410+3nol04@users.noreply.github.com>
2024-11-07 09:27:55 +0700

    Update KamarController.php

--- app/Http/Controllers/KamarController.php
+++ app/Http/Controllers/KamarController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Kamar;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class KamarController extends Controller
